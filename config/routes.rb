3dc5d071969585524e475b10600047a8c2650760
Your Name <you@example.com>
2023-01-28 13:49:50 -0300

    layout da tela de registro feita

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
  get "about", to:"about#index" #fazendo a rota about que aponta para os controllers
 
  get "sign_up", to: "registrations#new"
+ post "sign_up", to: "registrations#create"
 
  root to:"main#index" #rota raiz
 
